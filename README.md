3e50b892a092f83cb690ef8cb2eec81294b66e1c
Oved Fiso <ovedfs@gmail.com>
2024-03-08 10:15:40 -0600

    Ejercicio Tokenizer

---
+++ README.md
@@ -0,0 +1,54 @@
+# Ejercicio del Bootcamp de Ciencias de la Computación - Tokenizer
+## _Tokenizer_
+
+## fiso language
+
+### Tipos de Tokens
+- **INT**: 123, 78, 8, 9
+- **VAR**: 'mi_var', 'a', 'x_1'
+- **OP**: +, -, *, /
+- **ASSIGN**: =
+
+### Programa de ejemplo (archivo program.txt)
+```sh
+5 + 3
+44 / 22
+1766 - 89
+mi_var = 5
+a = 11 + 2 * 3
+z1 = x_1 + y_1
+```
+
+### Prueba
+```sh
+py tokenizer.py program.txt
+```
+
+### Salida
+```sh
+[
+<INT, 5, 0, 0>, 
+<OP, +, 0, 2>, 
+<INT, 3, 0, 4>, 
+<INT, 44, 1, 0>,
+<OP, /, 1, 3>,
+<INT, 22, 1, 5>,
+<INT, 1766, 2, 0>,
+<OP, -, 2, 5>,
+<INT, 89, 2, 7>,
+<VAR, mi_var, 3, 0>,
+<ASSIGN, =, 3, 7>,
+<INT, 5, 3, 9>,
+<VAR, a, 4, 0>,
+<ASSIGN, =, 4, 2>,
+<INT, 11, 4, 4>,
+<OP, +, 4, 7>,
+<INT, 2, 4, 9>,
+<OP, *, 4, 11>,
+<INT, 3, 4, 13>,
+<VAR, z1, 5, 0>,
+<ASSIGN, =, 5, 3>,
+<VAR, x_1, 5, 5>,
+<OP, +, 5, 9>,
+<VAR, y_1, 5, 11>]
+```
---
+++ tokenizer.py
@@ -0,0 +1,49 @@
+import sys
+import re
+from lib.Token import Token
+
+class Program:
+	tokens = []
+
+	def __init__(self, code):
+		self.code = code
+
+	def __repr__(self):
+		return "\n".join([repr(item) for item in Token.tokens]) or ""
+
+	def tokenize(self):
+		with open(self.code, 'r') as file:
+			for num_line, line in enumerate(file):
+				i = 0
+				while i < len(line):
+					# Integers
+					if line[i] in '0123456789':
+						number = ''
+						column = i
+						while line[i] in '0123456789' and i < len(line):
+							number += line[i]
+							i += 1
+						Program.tokens.append(Token('INT', number, num_line, column))
+					# Operators
+					elif line[i] in '+-*/':
+						Program.tokens.append(Token('OP', line[i], num_line, i))
+					# Assign operator
+					elif line[i] == '=':
+						Program.tokens.append(Token('ASSIGN', '=', num_line, i))
+					# Variables
+					elif re.match(r'[a-z_]', line[i]):
+						name = line[i]
+						column = i
+						i += 1
+						while re.match(r'[a-z0-9_]', line[i]) and i < len(line):
+							name += line[i]
+							i += 1
+						Program.tokens.append(Token('VAR', name, num_line, column))
+					
+					i += 1
+
+code = sys.argv[1]
+
+program = Program(code)
+program.tokenize()
+print(program.tokens)
